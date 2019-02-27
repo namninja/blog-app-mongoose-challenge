91c6ce74e267d291f68910d89c355944702cc030
Nam Ninja <namninja@gmail.com>
2019-02-26 18:08:44 -0800

    Setting up for integration testing and heroku deployment

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blogs-api';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blogs-api';
+// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blogs-api';
+// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blogs-api';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://zero-admin:zero-admin@cluster0-pfvwv.mongodb.net/blogs-api?retryWrites=true';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb+srv://zero-admin:zero-admin@cluster0-pfvwv.mongodb.net/test-blogs-api?retryWrites=true';
 exports.PORT = process.env.PORT || 8080;
